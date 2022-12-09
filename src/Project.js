1bd32bfa188c1c5dfa0858cc5ba6f9c97badb6eb
BebekJK <kevinjk2802@gmail.com>
2022-12-09 11:54:40 +0700

    update-2

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,7 @@
 import react, {Component} from 'react';
 import './Project.css'
-
+import background from './preview2048.png';
+import background2 from './tvshow.png'
 class Project extends Component{
     render(){
         return(
@@ -8,10 +9,24 @@ class Project extends Component{
                 {this.props.makeTitle('MY PROJECTS')}
                 <div id='projectContentContainer'>
                     <div>
-                        <iframe src='https://bebekjk.github.io/2048'></iframe>
-                        <div>2048 Duplicate</div>
+                        <img className='projectPreview' src={background}></img>
+                        <div className='projectName'>2048 Duplicate (Desktop Only)</div>
+                        <div>
+                            <button className='projectLinkContainer'>
+                                <a className='projectLink' href='https://bebekjk.github.io/2048/'>View Project</a>
+                            </button>
+                        </div>
                     </div>
                     
+                    <div>
+                        <img className='projectPreview' src={background2}></img>
+                        <div className='projectName'>Tv Show Search</div>
+                        <div>
+                            <button className='projectLinkContainer'>
+                                <a className='projectLink' href='https://bebekjk.github.io/TvShow'>View Project</a>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
